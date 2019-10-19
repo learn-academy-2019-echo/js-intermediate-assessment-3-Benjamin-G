36b941e80838a599605db4ab5cc7691f7530b2b9
Ubuntu <ubuntu@ip-172-31-84-177.ec2.internal>
2019-10-19 00:55:37 +0000

    final commit

--- js-intermediate.js
+++ js-intermediate.js
@@ -4,6 +4,22 @@
 // Be sure to use all given test cases
 
 // 1. Write a function called getFib that returns the first 10 numbers of the Fibonacci sequence in an array. Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+const compFib = n => {
+  if(n <= 1){
+    return n
+  }
+  return compFib(n-1) + compFib(n-2)
+}
+
+const getFib = leng => {
+  let temp = []
+  for(let i = 1; i <= leng; i++){
+    temp.push(compFib(i))
+  }
+  return temp
+}
+
+console.log(getFib(10))
 
 
 
@@ -14,6 +30,8 @@ var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [7, 23, -823]
 
+const oddChecker = arr => arr.filter(elm => {if(typeof elm === 'number'){return elm % 2 !==0}})
+console.log(oddChecker(fullArr1))
 
 
 // 3. Given the object below, complete the console.log to find specific information:
@@ -29,13 +47,13 @@ var bicycle = {
 }
 
 // Log the type of bicycle:
-console.log()
+console.log(bicycle.type)
 
 // Log the bell:
-console.log()
+console.log(bicycle.gear[2])
 
 // Log the PSI:
-console.log()
+console.log(bicycle.wheels.specs[2])
 
 
 // 4. Write a function that takes in an array and returns an array in reverse order.
@@ -45,17 +63,30 @@ var originalArray1 = [1, 2, 3, 4, 5, 6, 7]
 var originalArray2 = ["9", "1", "o", "h", "c", "e"]
 // Expected output: ["e", "c", "h", "o", "1", "9"]
 
+const revArr = arr => {
+  console.log(arr.length)
+  let temp = []
+  for(let i = arr.length-1; i >= 0; i--){
+    temp.push(arr[i])
+  }
+  return temp
+}
+
+console.log(revArr(originalArray1))
+
 
 
 // 5. Write the code that would make this test pass.
 
-describe("multTwo", () => {
-    test ("returns an array with all the numbers multiplied by two", () => {
-        expect(multTwo([3, 4, 5])).toEqual([6, 8, 10])
-        expect(multTwo([23, -9, 0])).toEqual([46, -18, 0])
-        expect(multTwo([4.5, -4.5, 12])).toEqual([9, -9, 24])
-    })
-})
+const multTwo = arr => arr.map(x => x*2)
+
+// describe("multTwo", () => {
+//     test ("returns an array with all the numbers multiplied by two", () => {
+//         expect(multTwo([3, 4, 5])).toEqual([6, 8, 10])
+//         expect(multTwo([23, -9, 0])).toEqual([46, -18, 0])
+//         expect(multTwo([4.5, -4.5, 12])).toEqual([9, -9, 24])
+//     })
+// })
 
 
 
@@ -63,8 +94,8 @@ describe("multTwo", () => {
 
 var ourString = "Hello Learn Students!"
 // Expected output: 3
-
-
+const countL = string => string.split(' ').join('').split('').filter(letter => letter.toLowerCase() === 'l').length
+console.log(countL(ourString))
 
 // 7. Create a function that takes in a string of a single word and returns the middle letter of the word. If the word is an even number of letters, return the two middle letters.
 
@@ -73,9 +104,11 @@ var middleLetters1 = "hello"
 var middleLetters2 = "rhinoceros"
 // Expected output: “oc”
 
+const middle = string => string.length % 2 !== 0 ? string.slice(string.length/2-.5,string.length/2+.5) : string.slice(string.length/2-1,string.length/2+1)
 
+console.log(middle(middleLetters1))
 
-
+console.log(middle(middleLetters2))
 // 8. Write a program to get the area of a sphere using object classes. Create three spheres with different radi as test cases. Area of a sphere =  4πr^2 (four pi r squared)
 
 
@@ -92,7 +125,8 @@ var myCar = {
     transmission: "manual",
   }
 }
-
+const decon = ({specs}) => [specs.doors, specs.transmission]
+console.log(decon(myCar))
 
 
 // 10. STRETCH: Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
